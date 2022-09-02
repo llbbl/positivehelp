eb28504e986df6683699d79c2bd4009732ad3eba
Logan Lindquist <logan@llbbl.com>
2022-09-02 14:17:51 -0500

    add t3 app

---
+++ src/pages/test.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+
+const Test: NextPage = () => { 
+    return (
+    <>
+            <Head>
+                <title>test loading data</title>
+                <meta name="description" content="testing" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+    </>
+    );
+
+}
+
+export default Test;
